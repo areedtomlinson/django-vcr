755ab5cdab1dfb65f1c72007410f197e960a3ccd
areedtomlinson <dev1@carbonobjects.com>
2015-11-23 14:20:20 +0000

    Added outline of test runner

--- django_vcr/tests.py
+++ django_vcr/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PlaybookTestCase(TestCase):
+    def assertPlaybook(playbook):
+        # Run through playbook and assert that given interactions lead
+        # to given responses. Assumes the presence of a "transaction_list"
+        # array in the JSON file.
+        
+        
+    def assertPlaybook(playbook, transaction_list):
+        # Run through playbook, with transactions in the order given in 
+        # array "transaction_list".
+    
+    
